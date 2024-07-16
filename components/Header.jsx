3d3fd40518bc683ddd39997f1bec6808f762476c
ABISHEK <abideepu19.vrs@gmail.com>
2024-07-17 01:44:21 +0530

    Dodge img

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react"
-
+import dodge from "/dodge.png"
 export default function Header(){
     return (
         <div className="header-div">
-            <img src="dodge.png" className="iconPic"/>
+            <img src={dodge} className="iconPic"/>
             <h2>Custom Meme Generator</h2>
         </div>
     )
